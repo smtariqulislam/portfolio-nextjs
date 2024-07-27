1b4655da666dab5e0959dac2c150ec7e7cb86ea6
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2024-07-27 14:08:09 +0600

    header part complete

---
+++ app/work/page.jsx
@@ -0,0 +1,5 @@
+const Work = () => {
+  return <div>Work Page</div>;
+};
+
+export default Work;
---
+++ components/Header.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between ic">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Luke <span className="text-accent">.</span>
+          </h1>
+        </Link>
+        {/* deshtop nav  */}
+        <div className="hidden xl:flex">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+        {/* mobile nav */}
+        <div className="xl:hidden">mobile nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
